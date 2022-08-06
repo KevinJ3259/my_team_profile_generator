3ca2d0dd38e2a90e34ddaa7da94c1bf543734cbb
KevinJ3259 <kmjordan2009.kj@gmail.com>
2022-08-06 06:44:26 -0500

    final commit

--- src/engineer.js
+++ src/engineer.js
@@ -2,35 +2,36 @@ const Engineer = require("../lib/Engineer");
 const inquirer = require("inquirer");
 
 const engineerQuestions = [
-    {
-      type: "input",
-      name: "name",
-      message: "What is the engineer's name?",
-    },
-    {
-      type: "input",
-      name: "ID",
-      message: "What is the engineer's ID?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is the engineer's email?",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "What is the engineer's github username?",
-    },
-  ];
+  {
+    type: "input",
+    name: "name",
+    message: "What is the engineer's name?",
+  },
+  {
+    type: "input",
+    name: "ID",
+    message: "What is the engineer's ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the engineer's email?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the engineer's github username?",
+  },
+];
 
-  function addEngineer() {
-    return inquirer.prompt(engineerQuestions)
+function addEngineer() {
+  return inquirer
+    .prompt(engineerQuestions)
     .then(function (answers) {
       return new Engineer(answers.name, answers.ID, answers.email, answers.github);
     })
     .then(function (engineer) {
-      return(`<div class="card">
+      return `<div class="card">
           <div class="card-head">
             <div>${engineer.getName()}</div>
             <div>${engineer.getRole()}</div>
@@ -38,10 +39,10 @@ const engineerQuestions = [
           <div class="card-body">
             <div>ID: ${engineer.getId()}</div>
             <div>Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></div>
-            <div>Github Username: ${engineer.getGithub()}</div>
+            <div>Github Username: <a href="${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></div>
           </div>
-        </div>`);
+        </div>`;
     });
-  }
+}
 
-  module.exports = addEngineer;
+module.exports = addEngineer;
